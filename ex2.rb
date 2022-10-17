682c86a49c6e5042a3d7f64e7f8739367477caca
Sean Hsu <sahsu.mobi@gmail.com>
2022-10-17 23:12:34 +0800

    add ex2

---
+++ ex2.rb
@@ -0,0 +1,8 @@
+# A comment, this is so you can read you program later.
+# Anything after this # is ignored by ruby.
+
+puts 'I cound have code like this.' # and the comment after is ignored.
+# you can also use cmment to "disable" or comment out a piece of code:
+# puts " This won't run."
+
+puts 'This will run.'
