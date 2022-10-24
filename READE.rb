cc4e4923cefd287d14bce0968d73cf23bce9c9dd
Sean Hsu <sahsu.mobi@gmail.com>
2022-10-24 23:04:04 +0800

    update reademe with last ex ruby file mtime

--- READE.rb
+++ READE.rb
@@ -2,14 +2,17 @@ require 'erb'
 
 a = Dir.entries('.')
 b = a.find_all { |q| !File.zero? q }.sort.find_all { |q| q.include? 'rb' }
-total = (b.count - 1) / 52.0
+last = b.count - 1
+total = last / 52.0
 totalp = total.truncate(2) * 100
+fn = 'ex' + last.to_s + '.rb'
+date = File.mtime(fn)
 
 template = ERB.new <<~EOF
 
   <% puts "# Me and https://learnrubythehardway.org/." %>
   <% puts "" %>
-  <% puts "## Currently Status " +  totalp.to_s + "%" %>
+  <% puts "## Currently Status " +  totalp.to_s + "%, in " + date.to_s %>
   <% puts "" %>
   <%  for i in 1..b.count - 1 do %>
     <% puts "## ex" + i.to_s %>
